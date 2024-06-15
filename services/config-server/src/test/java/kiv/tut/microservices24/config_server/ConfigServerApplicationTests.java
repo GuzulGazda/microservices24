617342e880917fa286d0e836c4a552b4068c0b08
ihor <kalinchukihor@gmail.com>
2024-06-15 14:48:40 +0300

    Create the Configuration Server project

---
+++ services/config-server/src/test/java/kiv/tut/microservices24/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kiv.tut.microservices24.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
